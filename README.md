1decabc34776bde2ad1be91f8165608ee4241458
toghrul <info@azreco.az>
2019-10-18 18:39:57 +0400

    Added synthesizing speech direct from text parameter.

--- README.md
+++ README.md
@@ -16,9 +16,13 @@ pip install requests
 
 # Usage example:
 
-python client.py -t text/example-tr.txt -l tr-TR -i api_user_id -k api_token -o example-tr.wav  
+python client.py --input-type file -t text/example-tr.txt -l tr-TR -i api_user_id -k api_token -o example-tr.wav  
 
-In this example the script uploads 'example-tr.txt', synthesizes speech using our tr-TR text-to-speech and saves the resulting audio as 'example-tr.wav' when the synthesizing process finished.
+or
+
+python client.py --input-type text -t "any text" -l tr-TR -i api_user_id -k api_token -o example-tr.wav  
+
+In this example for input type 'file' the script uploads 'example-tr.txt', synthesizes speech using our tr-TR text-to-speech and saves the resulting audio as 'example-tr.wav' when the synthesizing process finished. For input type 'text' the script sends text to the server, synthesizes speech using our tr-TR text-to-speech and saves the resulting audio as 'example-tr.wav' when the synthesizing process finished. 
 
 
 # How to get user id and token?
--- client.py
+++ client.py
@@ -7,11 +7,12 @@ import requests
 
 
 class Synthesizer(object):
-    def __init__(self, api_id, api_token, lang, api_url='http://api.azreco.az/synthesize'):
+    def __init__(self, api_id, api_token, lang, api_url_file='http://api.azreco.az/synthesize', api_url_text='http://api.azreco.az/synthesize/text'):
         self.api_id = api_id
         self.api_token = api_token
         self.lang = lang
-        self.api_url = api_url
+        self.api_url_file = api_url_file
+        self.api_url_text = api_url_text
 
     def synthesize(self, opts):
         """
@@ -26,9 +27,22 @@ class Synthesizer(object):
             logging.error("Problem opening text file {}".format(opts.text))
             raise
 
-        lang = {"lang": opts.lang}
+        request = requests.post(self.api_url_file, files=files, params=params)
+        if request.status_code == 200:
+            return request.content
+        else:
+            logging.error("{}".format(request.text))
+            raise Exception("Text-to-speech failed.")
 
-        request = requests.post(self.api_url, files=files, params=params)
+    def synthesize_text(self, opts):
+        """
+        Upload a new text file to azreco to convert into the speech
+        If upload suceeds then this method will return the audio in wave format
+        """
+
+        params = {'api_id':int(self.api_id),'api_token': self.api_token, 'lang':self.lang, 'text':opts.text}
+
+        request = requests.post(self.api_url_text, params=params)
         if request.status_code == 200:
             return request.content
         else:
@@ -45,8 +59,10 @@ def parse_args():
         description='Convert your text into the speech through the Azreco text-to-speech API',
         formatter_class=ArgumentDefaultsHelpFormatter,
     )
+    parser.add_argument('--input-type', type=str, required=True,
+                        help="Type of input. Must be one of 'text' or 'file'.")
     parser.add_argument('-t', '--text', type=str, required=True,
-                        help="Text file to be processed")
+                        help="Text or text file to be processed")
     parser.add_argument('-o', '--output', type=str, required=True,
                         help="Output wave filename")
     parser.add_argument('-i', '--id', type=str, required=True,
@@ -69,8 +85,11 @@ def main():
     opts = parse_args()
 
     client = Synthesizer(opts.id, opts.token, opts.lang)
-
-    result = client.synthesize(opts)
+    result = None
+    if opts.input_type == "file":
+        result = client.synthesize(opts)
+    else:
+        result = client.synthesize_text(opts)
     with open(opts.output, "wb") as out:
         out.write(result)
     logging.info("Your audio has been written to file {}".format(opts.output))
