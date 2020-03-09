fdd036ba7033ab1880113476b09ce1a51e29184e
toghrul <info@voicen.com>
2020-03-09 13:21:21 +0400

    Added new REST method and new --tts-id option

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# AzReco Text To Speech API Python example
-Example python script to help you integrate with our text-to-speech API.
+# AzReco **Text To Speech** API python example
+Sample python script to help you integrate with our text-to-speech API.
 
-This is an example python script for uploading text file and saving the audio into a .wav file.
+This is a sample python script for uploading text file and saving the audio into a .wav file.
 
 # Supporting languages
-AZERBAIJANI (az-AZ)
+Azerbaijani (az-AZ)
 
-TURKISH  (tr-TR)
+Turkish  (tr-TR)
+
+Russian  (ru-Ru)
 
 # Requirements
 
@@ -16,11 +18,29 @@ pip install requests
 
 # Usage example:
 
-python client.py --input-type file -t text/example-tr.txt -l tr-TR -i api_user_id -k api_token -o example-tr.wav  
+python client.py --input-type file -t text/example-tr.txt -l tr-TR -i api_user_id -k api_token --tts-id tts_id -o example-tr.wav  
 
 or
 
-python client.py --input-type text -t "any text" -l tr-TR -i api_user_id -k api_token -o example-tr.wav  
+python client.py --input-type text -t "any text" -l tr-TR -i api_user_id -k api_token --tts-id tts_id -o example-tr.wav  
+
+# What is --tts-id option?
+
+We have several voices for any language. Every voice has own identification. You can specify TTS identification with this option to get your audio file in different voices.
+
+# How to get voice identifications?
+
+We added new REST get method http://api.azreco.az/voices?api_id=YOUR_API_ID&api_token=YOUR_API_TOKEN. You can call this method in any browser. We also added this into the **Synthesizer** class
+as a method. The result is JSON array of voice informations. For example:
+```json
+[   {
+      "id":TTS_ID,
+      "ttsName":"VOICE_NAME",
+      "ttsLanguage":"SHORT_LANGUAGE",
+      "ttsGender":"GENDER"
+		}
+]
+```
 
 In this example for input type 'file' the script uploads 'example-tr.txt', synthesizes speech using our tr-TR text-to-speech and saves the resulting audio as 'example-tr.wav' when the synthesizing process finished. For input type 'text' the script sends text to the server, synthesizes speech using our tr-TR text-to-speech and saves the resulting audio as 'example-tr.wav' when the synthesizing process finished. 
 
--- client.py
+++ client.py
@@ -7,27 +7,32 @@ import requests
 
 
 class Synthesizer(object):
-    def __init__(self, api_id, api_token, lang, api_url_file='http://api.azreco.az/synthesize', api_url_text='http://api.azreco.az/synthesize/text'):
+    def __init__(self, api_id, api_token, lang, api_url_file='http://api.azreco.az/synthesize', api_url_text='http://api.azreco.az/synthesize/text', api_url_voices='http://api.azreco.az/voices'):
         self.api_id = api_id
         self.api_token = api_token
         self.lang = lang
         self.api_url_file = api_url_file
         self.api_url_text = api_url_text
+        self.api_url_voices = api_url_voices
 
     def synthesize(self, opts):
         """
         Upload a new text file to azreco to convert into the speech
         If upload suceeds then this method will return the audio in wave format
         """
-
+        print("Synthesizing... ", self.api_url_file)
         params = {'api_id':int(self.api_id),'api_token': self.api_token, 'lang':self.lang}
+        if opts.tts_id is not None:
+            params['tts_id'] = opts.tts_id
+
         try:
-            files={'file':open(opts.text, "rb")}
+            text_file = open(opts.text, 'rb');
         except IOError as ex:
             logging.error("Problem opening text file {}".format(opts.text))
             raise
-
+        files={'file':text_file}
         request = requests.post(self.api_url_file, files=files, params=params)
+        text_file.close()
         if request.status_code == 200:
             return request.content
         else:
@@ -41,6 +46,8 @@ class Synthesizer(object):
         """
 
         params = {'api_id':int(self.api_id),'api_token': self.api_token, 'lang':self.lang, 'text':opts.text}
+        if opts.tts_id is not None:
+            params['tts_id'] = opts.tts_id
 
         request = requests.post(self.api_url_text, params=params)
         if request.status_code == 200:
@@ -48,6 +55,19 @@ class Synthesizer(object):
         else:
             logging.error("{}".format(request.text))
             raise Exception("Text-to-speech failed.")
+
+    def get_voices(self, opts):
+        """
+        Gets list of information of voices supported by AzReco TTS service. The result is JSON array string.
+        """
+        params = {'api_id':int(self.api_id),'api_token': self.api_token}
+
+        request = requests.get(self.api_url_voices, params=params)
+        if request.status_code == 200:
+            return request.json()
+        else:
+            logging.error("{}".format(request.text))
+            raise Exception("Getting voices failed.")
 
 def parse_args():
     """
@@ -71,6 +91,7 @@ def parse_args():
                         help="Your text-to-speech API Token")
     parser.add_argument('-l', '--lang', type=str, required=True,
                         help="Code of language to use (e.g., az-AZ, tr-TR)")
+    parser.add_argument('--tts-id', type=str, required=False, help='Identificator of voice for given language. To see identificator of voices call get_voices() method of Synthesizer class.')
     parsed = parser.parse_args()
 
     return parsed
@@ -80,11 +101,13 @@ def main():
     """
     Example way to use the Azreco Client to convert you text into the speech
     """
+
     logging.basicConfig(level=logging.INFO)
 
     opts = parse_args()
 
     client = Synthesizer(opts.id, opts.token, opts.lang)
+    #print(client.get_voices(opts))
     result = None
     if opts.input_type == "file":
         result = client.synthesize(opts)
